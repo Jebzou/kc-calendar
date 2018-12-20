71603c63948004528e920b3d4dafdb27386245c0
Jebzou <22751386+Jebzou@users.noreply.github.com>
2018-12-20 08:40:29 +0100

    first push

---
+++ js/index.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+
+    $('#calendar').fullCalendar({
+      header: {
+        left: 'prev,next today',
+        center: 'title',
+        right: 'month,agendaWeek,agendaDay,listWeek'
+      },
+      defaultDate: '2018-03-12',
+      editable: true,
+      navLinks: true, // can click day/week names to navigate views
+      eventLimit: true, // allow "more" link when too many events
+      events: EVENTS,
+      loading: function(bool) {
+        $('#loading').toggle(bool);
+      }
+    });
+
+  });
